c5304f1badc83b0efb09aaf21145f7a1f7ac8246
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-11-23 20:38:13 +0700

    feat: api read shop recommendation limit

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -16,4 +16,20 @@ class ShopController extends Controller
             'data' => $shops
         ], 200);
     }
+
+    public function readRecommendationLimit()
+    {
+        $shops = Shop::orderBy('rate', 'desc')->limit(5)->get();
+
+        if (count($shops) > 0) {
+            return response()->json([
+                'data' => $shops
+            ], 200);
+        } else {
+            return response()->json([
+                'data' => $shops,
+                'message' => 'Not Found'
+            ], 404);
+        }
+    }
 }
